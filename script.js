7835ff484d97b06f2c27ce7db822a67b27294864
Ruslan <nooness@gmail.com>
2021-12-25 23:45:42 +0300

    after storm

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 'use strict';
 
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-var money = "ваш бюджет на месяц?";
-prompt ("money");
-var time = "Введите дату в формате YYYY-MM-DD";
-prompt ("time");
+let appData = {
+    butget: money,
+    expences: {},
+    optionalExpences: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?",''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?",'');
+
+appData.expences.a1 = a2;
+appData.expences.a2 = a4;
+
+alert (appData.budget / 30); 
